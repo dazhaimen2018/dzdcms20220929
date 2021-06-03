b8739f85bb24ce135e562f5477104fade734e7b7
TopAdmin <8355763@qq.com>
2021-06-04 07:46:31 +0800

    友情连接增加演示数据

---
+++ application/links/install/demo.sql
@@ -0,0 +1,3 @@
+INSERT INTO `yzn_links` (`id`, `linktype`, `url`, `name`, `image`, `target`, `description`, `inputtime`, `listorder`, `termsid`, `site_id`, `status`) VALUES
+(1, 0, 'https://www.dzdcms.com/', '多站点CMS内容管理系统', 0, '', '', 1615847076, 100, 0, '1,2', 1),
+(2, 0, 'https://www.topadmin.cn/', 'TopAdmin极速后台开发框架', 0, '', '', 1615847095, 100, 0, '1,2', 1);
---
+++ application/links/install/install.php
@@ -0,0 +1,50 @@
+<?php
+// +----------------------------------------------------------------------
+// | Yzncms [ 御宅男工作室 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 http://yzncms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 御宅男 <530765310@qq.com>
+// +----------------------------------------------------------------------
+
+// +----------------------------------------------------------------------
+// | 安装脚本
+// +----------------------------------------------------------------------
+namespace app\links\install;
+
+use think\Db;
+use util\Sql;
+use \sys\InstallBase;
+
+class install extends InstallBase
+{
+    /**
+     * 安装完回调
+     * @return boolean
+     */
+    public function end()
+    {
+
+        //安装演示数据
+        if (request()->param('demo') == 1) {
+            $sql_file = APP_PATH . "links/install/demo.sql";
+            if (file_exists($sql_file)) {
+                $sql_statement = Sql::getSqlFromFile($sql_file);
+                if (!empty($sql_statement)) {
+                    foreach ($sql_statement as $value) {
+                        try {
+                            Db::execute($value);
+                        } catch (\Exception $e) {
+                            $this->error = '导入演示数据失败，请检查demo.sql的语句是否正确';
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+}
