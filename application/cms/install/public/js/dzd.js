b913d526e86235f3d2bfdc8936009155fc0eb710
TopAdmin <8355763@qq.com>
2021-06-26 21:30:58 +0800

    完善默认模版

--- application/cms/install/public/js/dzd.js
+++ application/cms/install/public/js/dzd.js
@@ -36,3 +36,25 @@ function setLang(lang){
     $.cookie('lang', lang);
     window.location.reload();
 }
+
+//视频播放
+$('.videolist').each(function(){ //遍历视频列表
+    $(this).hover(function(){ //鼠标移上来后显示播放按钮
+        $(this).find('.videoed').show();
+    },function(){
+        $(this).find('.videoed').hide();
+    });
+    $(this).click(function(){ //这个视频被点击后执行
+        var img = $(this).attr('vpath');//获取视频预览图
+        var video = $(this).attr('ipath');//获取视频路径
+        $('.videos').html("<video id=\"video\" poster='"+img+"' style='width: 640px' src='"+video+"' preload=\"auto\" controls=\"controls\" autoplay=\"autoplay\"></video> <i onclick=\"closes()\"  class=\"layui-icon layui-icon-close vclose\"></i>");
+        $('.videos').show();
+    });
+});
+
+function closes(){
+    var v = document.getElementById('video');//获取视频节点
+    $('.videos').hide();//点击关闭按钮关闭暂停视频
+    v.pause();
+    $('.videos').html();
+}
