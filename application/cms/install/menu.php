9c54567242bbc70987160921446ef164c34ad9fc
TopAdmin <8355763@qq.com>
2021-06-30 08:28:29 +0800

    update

--- application/cms/install/menu.php
+++ application/cms/install/menu.php
@@ -105,7 +105,7 @@ return [
                     [
                         "route"  => "cms/publish/index",
                         "type"   => 1,
-                        "status" => 0,
+                        "status" => 1,
                         "name"   => "稿件管理",
                         "icon"   => "icon-draft-line",
                         "child"  => [
