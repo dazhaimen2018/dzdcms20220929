209d30b77321443ea00f2f3b84f9e0a021b96983
TopAdmin <8355763@qq.com>
2021-09-04 20:36:08 +0800

    update

--- update/20210904.sql
+++ update/20210904.sql
@@ -1,2 +1,2 @@
 -- 所有老站必须升级
-ALTER TABLE `yzn_site` ADD `alone` TINYINT NOT NULL DEFAULT '1' COMMENT '独立数据' AFTER `listorder`;
+ALTER TABLE `yzn_site` ADD `alone` TINYINT NOT NULL DEFAULT '1' COMMENT '真实数据' AFTER `listorder`;
