4497c45a4ffd26dbbff1408c7d419574669ffb85
TopAdmin <8355763@qq.com>
2021-09-20 18:17:23 +0800

    update

--- config/version.php
+++ config/version.php
@@ -15,6 +15,6 @@
 
 return [
     "yzncms_name"    => "DZDCMS多站点内容管理系统", //产品名称
-    "yzncms_release" => 20210914, //产品更新日期
-    "yzncms_version" => "2.3.0", //产品版本号
+    "yzncms_release" => 20210920, //产品更新日期
+    "yzncms_version" => "2.3.1", //产品版本号
 ];
