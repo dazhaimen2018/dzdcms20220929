2cf3eb665593e88d667bdc32b00c4b7b274741fb
TopAdmin <8355763@qq.com>
2021-10-11 09:19:40 +0800

    默认属性开启

--- application/cms/model/Models.php
+++ application/cms/model/Models.php
@@ -296,7 +296,6 @@ EOF;
                 'type'    => 'checkbox',
                 'listorder'=> 3,
                 'iffixed' => 0,
-                'status'  => 0,
                 'setting' => "a:3:{s:6:\"define\";s:31:\"varchar(32) NOT NULL DEFAULT ''\";s:7:\"options\";s:76:\"1:置顶[1]\r\n2:头条[2]\r\n3:特荐[3]\r\n4:推荐[4]\r\n5:热点[5]\r\n6:幻灯[6]\";s:5:\"value\";s:0:\"\";}",
             ],
             [
