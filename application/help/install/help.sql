87cf9125573f81a3385822ffb7224fbe1f1e219e
TopAdmin <8355763@qq.com>
2021-11-02 14:38:25 +0800

    帮助模块数据库前缀

--- application/help/install/help.sql
+++ application/help/install/help.sql
@@ -1,5 +1,4 @@
-DROP TABLE IF EXISTS `yzn_help`;
-CREATE TABLE `yzn_help` (
+CREATE TABLE IF NOT EXISTS `__PREFIX__help` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '栏目ID',
   `catname` varchar(30) NOT NULL DEFAULT '' COMMENT '栏目名称',
   `catdir` varchar(30) NOT NULL DEFAULT '' COMMENT '唯一标识',
@@ -21,8 +20,7 @@ CREATE TABLE `yzn_help` (
   UNIQUE KEY `catdir` (`catdir`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='栏目表';
 
-DROP TABLE IF EXISTS `yzn_help_priv`;
-CREATE TABLE `yzn_help_priv` (
+CREATE TABLE IF NOT EXISTS `__PREFIX__help_priv` (
  `catid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '栏目ID',
  `roleid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '角色或者组ID',
  `is_admin` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否为管理员 1、管理员',
