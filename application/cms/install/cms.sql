1eee9a0500630ff0a63fefa12ac609339a961ca2
TopAdmin <8355763@qq.com>
2021-11-24 18:43:59 +0800

    mysql编码

--- application/cms/install/cms.sql
+++ application/cms/install/cms.sql
@@ -26,136 +26,137 @@ PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='站点表';
 
 CREATE TABLE IF NOT EXISTS `__PREFIX__category` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '栏目ID',
-  `catname` varchar(100) NOT NULL DEFAULT '' COMMENT '栏目名称',
-  `catdir` varchar(100) NOT NULL DEFAULT '' COMMENT '唯一标识',
-  `english` varchar(100) NOT NULL DEFAULT '' COMMENT '英文标题',
-  `type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '类别',
-  `modelid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '模型ID',
-  `parentid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '父ID',
-  `arrparentid` varchar(255) NOT NULL DEFAULT '' COMMENT '所有父ID',
-  `arrchildid` mediumtext COMMENT '所有子栏目ID',
-  `sites` mediumtext COMMENT '所属站点',
-  `child` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否存在子栏目，1存在',
-  `image` varchar(255) NOT NULL DEFAULT '' COMMENT '栏目图片',
-  `icon` varchar(255) NOT NULL DEFAULT '' COMMENT '栏目图标',
-  `url` varchar(100) NOT NULL DEFAULT '' COMMENT '链接地址',
-  `items` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '文档数量',
-  `setting` text COMMENT '相关配置信息',
-  `listorder` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
-  `target` tinyint(2) NOT NULL DEFAULT '0' COMMENT '新窗口打开',
-  `status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `catdir` (`catdir`),
-  KEY `parentid` (`parentid`)
+`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '栏目ID',
+`catname` varchar(100) NOT NULL DEFAULT '' COMMENT '栏目名称',
+`catdir` varchar(100) NOT NULL DEFAULT '' COMMENT '唯一标识',
+`english` varchar(100) NOT NULL DEFAULT '' COMMENT '英文标题',
+`type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '类别',
+`modelid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '模型ID',
+`parentid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '父ID',
+`arrparentid` varchar(255) NOT NULL DEFAULT '' COMMENT '所有父ID',
+`arrchildid` mediumtext COMMENT '所有子栏目ID',
+`sites` mediumtext COMMENT '所属站点',
+`child` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否存在子栏目，1存在',
+`image` varchar(255) NOT NULL DEFAULT '' COMMENT '栏目图片',
+`icon` varchar(255) NOT NULL DEFAULT '' COMMENT '栏目图标',
+`url` varchar(100) NOT NULL DEFAULT '' COMMENT '链接地址',
+`items` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '文档数量',
+`setting` text COMMENT '相关配置信息',
+`listorder` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+`target` tinyint(2) NOT NULL DEFAULT '0' COMMENT '新窗口打开',
+`status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态',
+PRIMARY KEY (`id`),
+UNIQUE KEY `catdir` (`catdir`),
+KEY `parentid` (`parentid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='栏目表';
 
 CREATE TABLE IF NOT EXISTS `__PREFIX__category_data` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `catid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '栏目ID',
-  `catname` varchar(100) NOT NULL DEFAULT '' COMMENT '栏目名称',
-  `description` mediumtext NOT NULL COMMENT '栏目描述',
-  `setting` text COMMENT '相关配置信息',
-  `site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
-  `detail` mediumtext NOT NULL COMMENT '栏目介绍',
-  `status` tinyint(2) NOT NULL DEFAULT '1' COMMENT '是否导航显示',
-  PRIMARY KEY (`id`)
+`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+`catid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '栏目ID',
+`catname` varchar(100) NOT NULL DEFAULT '' COMMENT '栏目名称',
+`description` mediumtext NOT NULL COMMENT '栏目描述',
+`setting` text COMMENT '相关配置信息',
+`site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
+`detail` mediumtext NOT NULL COMMENT '栏目介绍',
+`status` tinyint(2) NOT NULL DEFAULT '1' COMMENT '是否导航显示',
+PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='栏目附表';
 
 CREATE TABLE IF NOT EXISTS `__PREFIX__category_priv` (
-  `catid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '栏目ID',
-  `roleid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '角色或者组ID',
-  `is_admin` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否为管理员 1、管理员',
-  `action` varchar(30) NOT NULL DEFAULT '' COMMENT '动作',
-  KEY `catid` (`catid`,`roleid`,`is_admin`,`action`)
+`catid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '栏目ID',
+`roleid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '角色或者组ID',
+`is_admin` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否为管理员 1、管理员',
+`action` varchar(30) NOT NULL DEFAULT '' COMMENT '动作',
+KEY `catid` (`catid`,`roleid`,`is_admin`,`action`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='栏目权限表';
 
 CREATE TABLE IF NOT EXISTS `__PREFIX__page` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `catid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '栏目ID',
-  `site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
-  `title` varchar(160) NOT NULL DEFAULT '' COMMENT '标题',
-  `thumb` varchar(160) NOT NULL DEFAULT '' COMMENT '单页图片',
-  `keywords` varchar(255) NOT NULL DEFAULT '' COMMENT '关键字',
-  `description` varchar(500) NOT NULL DEFAULT '' COMMENT 'SEO描述',
-  `content` text COMMENT '内容',
-  `inputtime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `updatetime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
-  PRIMARY KEY (`id`)
+`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+`catid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '栏目ID',
+`site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
+`title` varchar(160) NOT NULL DEFAULT '' COMMENT '标题',
+`thumb` varchar(160) NOT NULL DEFAULT '' COMMENT '单页图片',
+`keywords` varchar(255) NOT NULL DEFAULT '' COMMENT '关键字',
+`description` varchar(500) NOT NULL DEFAULT '' COMMENT 'SEO描述',
+`content` text COMMENT '内容',
+`inputtime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+`updatetime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='单页内容表';
 
 CREATE TABLE IF NOT EXISTS `__PREFIX__tags` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT 'tagID',
-  `tag` varchar(100) NOT NULL DEFAULT '' COMMENT 'tag名称',
-  `site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
-  `tagdir` varchar(255) NOT NULL DEFAULT '' COMMENT 'tag标识',
-  `seo_title` varchar(255) NOT NULL DEFAULT '' COMMENT 'seo标题',
-  `seo_keyword` varchar(255) NOT NULL DEFAULT '' COMMENT 'seo关键字',
-  `seo_description` varchar(255) NOT NULL DEFAULT '' COMMENT 'seo简介',
-  `usetimes` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '信息总数',
-  `hits` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '点击数',
-  `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '添加时间',
-  `update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
-  `listorder` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `tag` (`tag`),
-  KEY `usetimes` (`usetimes`,`listorder`),
-  KEY `hits` (`hits`,`listorder`)
+`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT 'tagID',
+`tag` varchar(100) NOT NULL DEFAULT '' COMMENT 'tag名称',
+`site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
+`tagdir` varchar(255) NOT NULL DEFAULT '' COMMENT 'tag标识',
+`seo_title` varchar(255) NOT NULL DEFAULT '' COMMENT 'seo标题',
+`seo_keyword` varchar(255) NOT NULL DEFAULT '' COMMENT 'seo关键字',
+`seo_description` varchar(255) NOT NULL DEFAULT '' COMMENT 'seo简介',
+`usetimes` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '信息总数',
+`hits` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '点击数',
+`create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '添加时间',
+`update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+`listorder` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+PRIMARY KEY (`id`),
+UNIQUE KEY `tag` (`tag`),
+KEY `usetimes` (`usetimes`,`listorder`),
+KEY `hits` (`hits`,`listorder`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='tags主表';
 
 CREATE TABLE IF NOT EXISTS `__PREFIX__tags_content` (
-  `tag` varchar(100) NOT NULL COMMENT 'tag名称',
-  `modelid` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '模型ID',
-  `contentid` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '信息ID',
-  `catid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '栏目ID',
-  `site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
-  `updatetime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
-  KEY `modelid` (`modelid`,`contentid`),
-  KEY `tag` (`tag`(10))
+`tag` varchar(100) NOT NULL COMMENT 'tag名称',
+`modelid` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '模型ID',
+`contentid` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '信息ID',
+`catid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '栏目ID',
+`site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
+`updatetime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+KEY `modelid` (`modelid`,`contentid`),
+KEY `tag` (`tag`(10))
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='tags数据表';
 
 CREATE TABLE IF NOT EXISTS `__PREFIX__lang` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '配置ID',
-  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '配置名称',
-  `type` varchar(100) NOT NULL DEFAULT '' COMMENT '配置类型',
-  `title` varchar(100) NOT NULL DEFAULT '' COMMENT '配置标题',
-  `group` varchar(100) NOT NULL DEFAULT '' COMMENT '配置分组',
-  `options` varchar(255) NOT NULL DEFAULT '' COMMENT '配置项',
-  `remark` varchar(255) NOT NULL DEFAULT '' COMMENT '配置说明',
-  `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
-  `value` text COMMENT '相关配置信息',
-  `listorder` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
-  `status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态',
-  PRIMARY KEY (`id`)
+`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '配置ID',
+`name` varchar(100) NOT NULL DEFAULT '' COMMENT '配置名称',
+`type` varchar(100) NOT NULL DEFAULT '' COMMENT '配置类型',
+`title` varchar(100) NOT NULL DEFAULT '' COMMENT '配置标题',
+`group` varchar(100) NOT NULL DEFAULT '' COMMENT '配置分组',
+`options` varchar(255) NOT NULL DEFAULT '' COMMENT '配置项',
+`remark` varchar(255) NOT NULL DEFAULT '' COMMENT '配置说明',
+`create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+`update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+`value` text COMMENT '相关配置信息',
+`listorder` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+`status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态',
+PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='碎片管理';
 
 CREATE TABLE IF NOT EXISTS `__PREFIX__lang_data` (
- `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
- `lang_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '配置ID',
- `value` text COMMENT '相关配置信息',
- `site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
- `status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态',
- PRIMARY KEY (`id`)
+`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+`lang_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '配置ID',
+`value` text COMMENT '相关配置信息',
+`site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
+`status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态',
+PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='网站配置附表';
 
 CREATE TABLE IF NOT EXISTS `dzd_search_log` (
-    `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-    `site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
-    `keywords` varchar(255) NOT NULL DEFAULT '' COMMENT '关键字',
-    `nums` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '点击数',
-    `ip` varchar(30) NOT NULL DEFAULT '' COMMENT 'SEO描述',
-    `inputtime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-    `updatetime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
-    PRIMARY KEY (`id`)
-    ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='搜索记录表';
+`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+`site_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '站点ID',
+`keywords` varchar(50) NOT NULL DEFAULT '' COMMENT '关键字',
+`nums` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '点击数',
+`ip` varchar(30) NOT NULL DEFAULT '' COMMENT 'SEO描述',
+`inputtime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+`updatetime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+PRIMARY KEY (`id`),
+UNIQUE KEY `keywords` (`keywords`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='搜索记录表';
 
 CREATE TABLE IF NOT EXISTS `__PREFIX__lang_group` (
-  `id` mediumint UNSIGNED NOT NULL COMMENT '碎片分组id,自增主键',
-  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '碎片分组',
-  `description` varchar(80) NOT NULL DEFAULT '' COMMENT '描述信息',
-  `status` tinyint NOT NULL DEFAULT '0' COMMENT '状态',
-  PRIMARY KEY (`id`)
+`id` mediumint UNSIGNED NOT NULL COMMENT '碎片分组id,自增主键',
+`name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '碎片分组',
+`description` varchar(80) NOT NULL DEFAULT '' COMMENT '描述信息',
+`status` tinyint NOT NULL DEFAULT '0' COMMENT '状态',
+PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='碎片分组表';
 
 
@@ -294,13 +295,13 @@ INSERT INTO `__PREFIX__site` (`id`, `name`, `mark`, `http`, `domain`, `url`, `lo
 (1, '中文站', 'zh-cn', 'http', 'demo.dzdcms.com', 'http://demo.dzdcms.com', '/uploads/images/logo.png', '/favicon.ico', 'default', '多站点', '多站点CMS演示站', '多站点CMS,多站点官网,多站点官方网站,DzdCMS模板,多站点模板,模块插件,开源,PHP CMS,PHP', '多站点CMS官方网站是集简单、健壮、灵活、开源几大特点的开源多站点内容管理系统,是国内开源CMS的站群系统，目前程序安装量已经非常高，很多外贸网站，集团网站、城市分站都在使用多站点CMS或基于CMS核心开发', 0, '', NULL, 0, 1, 1, 1, 0, 1, 0);
 
 ALTER TABLE `__PREFIX__site`
-    MODIFY `id` smallint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '站点ID', AUTO_INCREMENT=2;
+MODIFY `id` smallint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '站点ID', AUTO_INCREMENT=2;
 
 ALTER TABLE `__PREFIX__lang`
-    MODIFY `id` smallint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '配置ID', AUTO_INCREMENT=124;
+MODIFY `id` smallint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '配置ID', AUTO_INCREMENT=124;
 
 ALTER TABLE `__PREFIX__lang_group`
-    MODIFY `id` mediumint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户组id,自增主键', AUTO_INCREMENT=4;
+MODIFY `id` mediumint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户组id,自增主键', AUTO_INCREMENT=4;
 
 
 
