a1802fac6340f7fc7779c7d54617bc45804d4857
TopAdmin <8355763@qq.com>
2021-12-09 08:32:51 +0800

    sql

--- update/20211208.sql
+++ update/20211208.sql
@@ -1,13 +1,13 @@
 ALTER TABLE `dzd_site`
-    ADD `website` VARCHAR(100) NULL DEFAULT NULL COMMENT ''网站名称'' AFTER `source`,
-    ADD `company` VARCHAR(100) NULL DEFAULT NULL COMMENT ''公司名称'' AFTER `website`,
-    ADD `icp` VARCHAR(30) NULL DEFAULT NULL COMMENT ''ICP备案号'' AFTER `company`,
-    ADD `icp_link` VARCHAR(100) NULL DEFAULT NULL COMMENT ''ICP备案链接'' AFTER `icp`,
-    ADD `gwa` VARCHAR(30) NULL DEFAULT NULL COMMENT ''公安备案号'' AFTER `icp_link`,
-    ADD `gwa_link` VARCHAR(100) NULL DEFAULT NULL COMMENT ''公安备案链接'' AFTER `gwa`,
-    ADD `chat` VARCHAR(255) NULL DEFAULT NULL COMMENT ''客服代码'' AFTER `gwa_link`,
-    ADD `statistics` VARCHAR(255) NULL DEFAULT NULL COMMENT ''统计代码'' AFTER `chat`,
-    ADD `copyright` VARCHAR(255) NULL DEFAULT NULL COMMENT ''版权信息'' AFTER `statistics`;
+    ADD `website` VARCHAR(100) NULL  COMMENT '网站名称' AFTER `source`,
+    ADD `company` VARCHAR(100) NULL  COMMENT '公司名称' AFTER `website`,
+    ADD `icp` VARCHAR(30) NULL  COMMENT 'ICP备案号' AFTER `company`,
+    ADD `icp_link` VARCHAR(100) NULL  COMMENT 'ICP备案链接' AFTER `icp`,
+    ADD `gwa` VARCHAR(30) NULL  COMMENT '公安备案号' AFTER `icp_link`,
+    ADD `gwa_link` VARCHAR(100) NULL  COMMENT '公安备案链接' AFTER `gwa`,
+    ADD `chat` VARCHAR(255) NULL  COMMENT '客服代码' AFTER `gwa_link`,
+    ADD `statistics` VARCHAR(255) NULL  COMMENT '统计代码' AFTER `chat`,
+    ADD `copyright` VARCHAR(255) NULL  COMMENT '版权信息' AFTER `statistics`;
 
 ALTER TABLE `dzd_model_field`
     ADD `iflist` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否列表显示' AFTER `ifrequire`;
