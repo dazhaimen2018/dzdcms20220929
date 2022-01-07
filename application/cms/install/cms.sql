ff9997f1f91d2cf237444fe517738f4f7a3bece0
TopAdmin <8355763@qq.com>
2022-01-08 07:09:31 +0800

    修复数据库

--- application/cms/install/cms.sql
+++ application/cms/install/cms.sql
@@ -36,7 +36,7 @@ PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='站点表';
 
 CREATE TABLE IF NOT EXISTS `__PREFIX__site_domain` (
-`id` smallint(5) UNSIGNED NOT NULL COMMENT '自然ID',
+`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '栏目ID',
 `sites` smallint(4) NOT NULL DEFAULT '0' COMMENT '站点ID',
 `domain` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '域名',
 `master` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否主域名',
--- update/20220107.sql
+++ update/20220107.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `dzd_site` CHANGE `source` `master` TINYINT(4) NOT NULL DEFAULT '0' COMMENT '默认站点';
 
 CREATE TABLE `dzd_site_domain` (
-`id` smallint(5) UNSIGNED NOT NULL COMMENT '自然ID',
+`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '栏目ID',
 `sites` smallint(4) NOT NULL DEFAULT '0' COMMENT '站点ID',
 `domain` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '域名',
 `master` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否主域名',
