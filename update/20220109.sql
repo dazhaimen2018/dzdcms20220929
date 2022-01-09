5b830b76c3ab93fe87ff8641330030491eed8bcb
TopAdmin <8355763@qq.com>
2022-01-09 12:57:20 +0800

    专题属性字段修改

--- update/20220109.sql
+++ update/20220109.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS `dzd_special_data` (
     `sites` int(10) UNSIGNED DEFAULT '0' COMMENT '所属站点',
     `specid` smallint(5) UNSIGNED NOT NULL DEFAULT '0',
     `modelid` smallint(6) UNSIGNED DEFAULT '0',
-    `thumb` tinyint(1) NOT NULL DEFAULT '0',
-    `data` mediumtext,
+    `title` varchar(255) DEFAULT '' COMMENT '标题',
+    `thumb` varchar(255) DEFAULT '' COMMENT '图片',
     `listorder` mediumint(8) DEFAULT '0',
     PRIMARY KEY (`id`)
     ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='专题内容表';
@@ -19,8 +19,8 @@ CREATE TABLE IF NOT EXISTS `dzd_flag_data` (
     `sites` int(10) UNSIGNED DEFAULT '0' COMMENT '所属站点',
     `flagid` smallint(5) UNSIGNED NOT NULL DEFAULT '0',
     `modelid` smallint(6) UNSIGNED DEFAULT '0',
-    `thumb` tinyint(1) NOT NULL DEFAULT '0',
-    `data` mediumtext,
+    `title` varchar(255) DEFAULT '' COMMENT '标题',
+    `thumb` varchar(255) DEFAULT '' COMMENT '图片',
     `listorder` mediumint(8) DEFAULT '0',
     PRIMARY KEY (`id`)
     ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='推荐位内容表';
